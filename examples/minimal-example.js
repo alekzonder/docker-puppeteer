3aa605be5cf4e0d27703e96c29aeb073c691f2c8
Anish Karandikar <anishkny@gmail.com>
2018-09-11 10:33:37 -0400

    Update minimal-example.js

--- examples/minimal-example.js
+++ examples/minimal-example.js
@@ -2,10 +2,9 @@ const puppeteer = require('puppeteer');
 
 (async() => {
 
-  const browser = await puppeteer.launch({ args: [
-    '--no-sandbox',
-    '--disable-setuid-sandbox'
-  ]});
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  });
   const page = await browser.newPage();
   await page.goto('https://www.google.com/', { waitUntil: 'networkidle2' });
   await browser.close();
