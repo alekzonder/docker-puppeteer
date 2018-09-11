7399e0a34daa7c5ca44bdd6cbb5e9d7a27b19560
Anish Karandikar <anishkny@gmail.com>
2018-09-11 10:21:04 -0400

    Create minimal-example.js
    [skip ci]

---
+++ examples/minimal-example.js
@@ -0,0 +1,13 @@
+const puppeteer = require('puppeteer');
+
+(async() => {
+
+  const browser = await puppeteer.launch({ args: [
+    '--no-sandbox',
+    '--disable-setuid-sandbox'
+  ]});
+  const page = await browser.newPage();
+  await page.goto('https://www.google.com/', { waitUntil: 'networkidle' });
+  await browser.close();
+  
+})();
